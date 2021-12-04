f69b5896481e6d54f26ff78f610fb5244cf4d8ac
Jude <jude@ai4gov.net>
2021-12-04 19:08:27 +0800

    initial commit

---
+++ src/pages/CreateForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <label>This is the page</label>
+    <FormBuilder v-model="configuration"></FormBuilder>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { FormBuilder } from 'v-form-builder'
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { FormBuilder },
+  data: () => ({
+    configuration: {}
+  })
+})
+</script>
